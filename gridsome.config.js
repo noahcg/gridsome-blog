fbdcb9dd9de3d677fde75799e589c5d7bf6526bd
nglushien17 <nglushien@ushmm.org>
2019-11-13 18:04:06 -0500

    updated path to posts in config file

--- gridsome.config.js
+++ gridsome.config.js
@@ -31,7 +31,7 @@ module.exports = {
     {
       use: "@gridsome/source-filesystem",
       options: {
-        path: "content/posts/**/*.md",
+        path: "content/**/*.md",
         typeName: "Post",
         refs: {
           tags: {
